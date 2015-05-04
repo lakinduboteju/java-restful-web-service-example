cdf5df93712235b6e59377cf8449a1f3806af337
Lakindu Boteju <lakinduboteju@gmail.com>
2015-05-04 12:26:33 +0700

    initial commit

---
+++ rest_certificate_security/src/main/java/my/rest/test/controller/TestController.java
@@ -0,0 +1,17 @@
+package my.rest.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	@RequestMapping( value = "/greet", method = RequestMethod.GET )
+	@ResponseBody
+	public String hello()
+	{
+		System.out.println("Hello!");
+		return "Hello!";
+	}
+}
